7163e64f2564117354ef0e25ca19e178949847d6
minchjung <78589751+minchjung@users.noreply.github.com>
2021-06-06 19:05:53 +0900

    Delete Menu01_own directory

--- Menu01_own/01.js
+++
@@ -1,8 +0,0 @@
-$(".menu li").mouseover(function(){
-    $(this).children(".submenu").css("display","block")
-    console.log("adsfasdf")
-})
-$(".menu li").mouseleave(function(){
-    $(this).children(".submenu").css("display","none")
-    console.log("adsfasdf")
-})
